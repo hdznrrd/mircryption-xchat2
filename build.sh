ebd9a7e5963d2eb6839b9b1d7c187b6e1d208999
hdznrrd <hadez@nrrd.de>
2013-07-13 17:37:04 +0200

    build wrapper

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OLD_FOLDER=$(pwd)
+MY_FOLDER=$(dirname $0)
+
+# fetch and unpack mircryption source
+cd $MY_FOLDER
+./3rdparty/fetch-mcps.sh
+
+# setup base build environment
+rm -rf build
+mkdir build
+cd build
+cp -r ../3rdparty/MircryptionExtras/FullSources/mircryption/src .
+cd src/xchat
+
+# patch build environment
+cp ../../../mircryption.cpp .
+
+make $1 && cp mircryption.so ../../

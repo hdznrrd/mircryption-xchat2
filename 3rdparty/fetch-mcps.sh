1ff6d3468c8be1ef8ce1c6e98bfbcb380d170240
hdznrrd <hadez@nrrd.de>
2013-07-13 17:37:21 +0200

    only fetching if not already fetched

--- 3rdparty/fetch-mcps.sh
+++ 3rdparty/fetch-mcps.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-rm -rf MircryptionExtras*
-wget http://www.donationcoder.com/Software/Mouser/mircryption/downloads/MircryptionExtras.zip
-unzip MircryptionExtras.zip
+OLD_FOLDER=$(pwd)
+MY_FOLDER=$(dirname $0)
+
+cd $MY_FOLDER
+
+if [ ! -d MircryptionExtras ]
+then
+	rm -rf MircryptionExtras*
+	wget http://www.donationcoder.com/Software/Mouser/mircryption/downloads/MircryptionExtras.zip
+	unzip MircryptionExtras.zip
+fi
+
+cd $OLD_FOLDER

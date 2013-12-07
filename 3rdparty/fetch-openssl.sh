4ea0cda1ffa4d9fa744156d74e5ba8b8b2df2fa8
hdznrrd <hadez@nrrd.de>
2013-12-07 12:46:43 +0100

    Feature: use system version of OpenSSL if present or download 1.0.1e otherwise

--- 3rdparty/fetch-openssl.sh
+++ 3rdparty/fetch-openssl.sh
@@ -5,8 +5,13 @@ MY_FOLDER=$(dirname $0)
 
 OPENSSL_VERSION=$1
 if [ "."$OPENSSL_VERSION == "." ]; then
-	OPENSSL_VERSION="1.0.1e"
-	echo "Defaulting to OpenSLL v$OPENSSL_VERSION"
+	OPENSSL_VERSION=$(openssl version | awk '{print $2}')
+	if [ "."$OPENSSL_VERSION == "."]; then
+		OPENSSL_VERSION="1.0.1e"
+		echo "Defaulting to OpenSLL v$OPENSSL_VERSION (hardcoded)"
+	else
+		echo "Defaulting to OpenSLL v$OPENSSL_VERSION (local system version)"
+	fi
 fi
 
 echo "Downloading OpenSSL v$OPENSSL_VERSION"
@@ -15,10 +20,13 @@ cd $MY_FOLDER
 
 if [ ! -d openssl-$OPENSSL_VERSION ]
 then
-	rm -rf openssl-$OPENSSL_VERSION*
+	rm -rf openssl-$OPENSSL_VERSION* openssl-active
 	wget http://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
 	tar xzvf openssl-$OPENSSL_VERSION.tar.gz
+	ln -s openssl-$OPENSSL_VERSION openssl-active
 else
+	rm -f openssl-active
+	ln -s openssl-$OPENSSL_VERSION openssl-active
 	echo "OpenSSL v$OPENSSL_VERSION already downloaded"
 fi
 
